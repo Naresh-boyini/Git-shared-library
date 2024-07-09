76d1d3a7226c1560bacc49b910f4d4b3838e2099
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-10 02:31:23 +0530

    Update Stages.groovy

--- src/org/example/Stages.groovy
+++ src/org/example/Stages.groovy
@@ -1,144 +1,125 @@
-package org.example;
+// src/org/example/Stages.groovy
 
-import static org.example.Common.*;
+package org.example
 
 class Stages {
-    static Closure checkout(Map params) {
-        return {
-            stage('Checkout') {
-                when {
-                    expression {
-                        def stagesToRun = splitStages(params.STAGES_TO_RUN)
-                        echo "Stages to run: ${stagesToRun}"
-                        return shouldRunStage('Checkout', stagesToRun)
-                    }
+    static def checkout(Map params) {
+        stage('Checkout') {
+            when {
+                expression {
+                    return shouldRunStage(params, 'Checkout')
                 }
-                steps {
-                    git branch: params.GIT_BRANCH, url: params.GIT_URL
+            }
+            steps {
+                git branch: "${params.GIT_BRANCH}", url: "${params.GIT_URL}"
+            }
+        }
+    }
+
+    static def modifyGoMod(Map params) {
+        stage('Modify go.mod') {
+            when {
+                expression {
+                    return shouldRunStage(params, 'Modify go.mod')
+                }
+            }
+            steps {
+                sh '''
+                export GO111MODULE=on
+                sed -i 's/go 1.20/go 1.18/g' go.mod
+                '''
+            }
+        }
+    }
+
+    static def build(Map params) {
+        stage('Build') {
+            when {
+                expression {
+                    return shouldRunStage(params, 'Build')
+                }
+            }
+            steps {
+                sh '''
+                go mod tidy
+                go mod download
+                go build -o employee-api .
+                '''
+            }
+        }
+    }
+
+    static def unitTest(Map params) {
+        stage('Unit Test') {
+            when {
+                expression {
+                    return shouldRunStage(params, 'Unit Test')
+                }
+            }
+            steps {
+                sh 'go test ./... -v | tee unit-test-results.xml'
+            }
+        }
+    }
+
+    static def sonarQubeAnalysis(Map params) {
+        stage('SonarQube Analysis') {
+            when {
+                expression {
+                    return shouldRunStage(params, 'SonarQube Analysis')
+                }
+            }
+            steps {
+                withSonarQubeEnv("${params.SONARQUBE_ENV_NAME}") {
+                    sh "${SCANNER_HOME}/bin/sonar-scanner"
                 }
             }
         }
     }
 
-    static Closure modifyGoMod(Map params) {
-        return {
-            stage('Modify go.mod') {
-                when {
-                    expression {
-                        def stagesToRun = splitStages(params.STAGES_TO_RUN)
-                        echo "Stages to run: ${stagesToRun}"
-                        return shouldRunStage('Modify go.mod', stagesToRun)
-                    }
+    static def dependencyScan(Map params) {
+        stage('Dependency Scan') {
+            when {
+                expression {
+                    return shouldRunStage(params, 'Dependency Scan')
                 }
-                steps {
-                    sh '''
-                    export GO111MODULE=on
-                    sed -i 's/go 1.20/go 1.18/g' go.mod
-                    '''
-                }
+            }
+            steps {
+                // Run OWASP Dependency-Check
+                dependencyCheck additionalArguments: '--scan . --format ALL', odcInstallation: "${params.DEPENDENCY_TOOL_NAME}"
             }
         }
     }
 
-    static Closure build(Map params) {
-        return {
-            stage('Build') {
-                when {
-                    expression {
-                        def stagesToRun = splitStages(params.STAGES_TO_RUN)
-                        echo "Stages to run: ${stagesToRun}"
-                        return shouldRunStage('Build', stagesToRun)
-                    }
+    static def archiveReport(Map params) {
+        stage('Archive Report') {
+            when {
+                expression {
+                    return shouldRunStage(params, 'Archive Report')
                 }
-                steps {
-                    sh '''
-                    go mod tidy
-                    go mod download
-                    go build -o employee-api .
-                    '''
-                }
+            }
+            steps {
+                // Archive the unit test results
+                archiveArtifacts artifacts: 'unit-test-results.xml', allowEmptyArchive: true
+
+                echo "Publishing Reports"
+                // Publish OWASP Dependency-Check report in HTML format
+                publishHTML([
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: true,
+                    keepAll: true,
+                    reportDir: '.',
+                    reportFiles: 'dependency-check-report.html',
+                    reportName: 'Dependency Check Report',
+                    reportTitles: ''
+                ])
             }
         }
     }
 
-    static Closure unitTest(Map params) {
-        return {
-            stage('Unit Test') {
-                when {
-                    expression {
-                        def stagesToRun = splitStages(params.STAGES_TO_RUN)
-                        echo "Stages to run: ${stagesToRun}"
-                        return shouldRunStage('Unit Test', stagesToRun)
-                    }
-                }
-                steps {
-                    sh 'go test ./... -v | tee unit-test-results.xml'
-                }
-            }
-        }
-    }
-
-    static Closure sonarQubeAnalysis(Map params) {
-        return {
-            stage('SonarQube Analysis') {
-                when {
-                    expression {
-                        def stagesToRun = splitStages(params.STAGES_TO_RUN)
-                        echo "Stages to run: ${stagesToRun}"
-                        return shouldRunStage('SonarQube Analysis', stagesToRun)
-                    }
-                }
-                steps {
-                    withSonarQubeEnv(params.SONARQUBE_ENV_NAME) {
-                        sh "${params.SCANNER_HOME}/bin/sonar-scanner"
-                    }
-                }
-            }
-        }
-    }
-
-    static Closure dependencyScan(Map params) {
-        return {
-            stage('Dependency Scan') {
-                when {
-                    expression {
-                        def stagesToRun = splitStages(params.STAGES_TO_RUN)
-                        echo "Stages to run: ${stagesToRun}"
-                        return shouldRunStage('Dependency Scan', stagesToRun)
-                    }
-                }
-                steps {
-                    dependencyCheck additionalArguments: '--scan . --format ALL', odcInstallation: params.DEPENDENCY_TOOL_NAME
-                }
-            }
-        }
-    }
-
-    static Closure archiveReport(Map params) {
-        return {
-            stage('Archive Report') {
-                when {
-                    expression {
-                        def stagesToRun = splitStages(params.STAGES_TO_RUN)
-                        echo "Stages to run: ${stagesToRun}"
-                        return shouldRunStage('Archive Report', stagesToRun)
-                    }
-                }
-                steps {
-                    archiveArtifacts artifacts: 'unit-test-results.xml', allowEmptyArchive: true
-
-                    echo "Publishing Reports"
-                    publishHTML([
-                        allowMissing: false,
-                        alwaysLinkToLastBuild: true,
-                        keepAll: true,
-                        reportDir: '.',
-                        reportFiles: 'dependency-check-report.html',
-                        reportName: 'Dependency Check Report',
-                        reportTitles: ''
-                    ])
-                }
-            }
-        }
+    static def shouldRunStage(Map params, String stageName) {
+        def stagesToRun = params.STAGES_TO_RUN.split(',').collect { it.trim() }
+        echo "Stages to run: ${stagesToRun}"
+        return stagesToRun.contains(stageName)
     }
 }
