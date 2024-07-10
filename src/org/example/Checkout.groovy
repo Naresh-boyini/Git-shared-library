f5c9278343938c992c711c8f195c46757d19a4b2
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-10 16:34:53 +0530

    Update Checkout.groovy

--- src/org/example/Checkout.groovy
+++ src/org/example/Checkout.groovy
@@ -2,17 +2,10 @@ package org.example
 
 class Checkout {
     static void execute(script) {
-        script.stage('Checkout') {
-            script.when {
-                script.expression {
-                    def stagesToRun = script.params.STAGES_TO_RUN.split(',').collect { it.trim() }
-                    script.echo "Stages to run: ${stagesToRun}"
-                    return stagesToRun.contains('Checkout')
-                }
-            }
-            script.steps {
-                script.git branch: "${script.params.GIT_BRANCH}", url: "${script.params.GIT_URL}"
-            }
-        }
+        script.checkout([
+            $class: 'GitSCM',
+            branches: [[name: script.params.GIT_BRANCH]],
+            userRemoteConfigs: [[url: script.params.GIT_URL]]
+        ])
     }
 }
