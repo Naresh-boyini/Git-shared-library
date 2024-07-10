bd230c499a348e9542f0943455e3a786895c2f72
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-10 17:13:58 +0530

    Update BuildPipeline.groovy

--- src/org/example/BuildPipeline.groovy
+++ src/org/example/BuildPipeline.groovy
@@ -1,57 +1,14 @@
 package org.example
 
-class BuildPipeline {
-    static void run(String gitUrl, String gitBranch) {
-        pipeline {
-            agent any
-            
-            stages {
-                stage('Checkout') {
-                    steps {
-                        script {
-                            Checkout.execute(script, gitUrl, gitBranch)
-                        }
-                    }
-                }
-
-                stage('Modify go.mod') {
-                    steps {
-                        script {
-                            Checkout.execute(script, gitUrl, gitBranch)
-                            sh '''
-                            export GO111MODULE=on
-                            sed -i 's/go 1.20/go 1.18/g' go.mod
-                            '''
-                        }
-                    }
-                }
-
-                stage('Build') {
-                    steps {
-                        script {
-                            Checkout.execute(script, gitUrl, gitBranch)
-                            def goTool = tool name: 'Go', type: 'Go'
-                            env.PATH = "${goTool}/bin:${env.PATH}"
-                            
-                            sh '''
-                            go mod tidy
-                            go mod download
-                            go build -o employee-api .
-                            '''
-                        }
-                    }
-                }
-            }
-
-            post {
-                success {
-                    echo 'Build successful!'
-                    // Additional actions on success
-                }
-                failure {
-                    echo 'Build failed!'
-                    // Additional actions on failure
-                }
+class Build {
+    static void execute(script, String projectDirectory) {
+        script.withEnv(['PATH+GO': "${tool name: 'Go', type: 'Go'}/bin"]) {
+            script.dir(projectDirectory) {
+                script.sh '''
+                go mod tidy
+                go mod download
+                go build -o employee-api .
+                '''
             }
         }
     }
