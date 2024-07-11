b3a3c5d571f8b6ab460fb4308f4d27d0dad5944b
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-12 02:51:21 +0530

    Update HelloWorld.groovy

--- src/org/example/HelloWorld.groovy
+++ src/org/example/HelloWorld.groovy
@@ -3,8 +3,23 @@ package org.example
 
 class HelloWorld {
     static void sayHello() {
-        def command = "echo 'Hello, World!'"
+        // Command to create a Python virtual environment
+        def createEnvCommand = "python3 -m venv myenv"
+        
+        // Command to activate the virtual environment
+        def activateEnvCommand = ". myenv/bin/activate"
+        
+        // Command to run Snyk security test
+        def snykTestCommand = "snyk test --file=poetry.lock"
+        
+        // Execute commands sequentially
+        executeShellCommand(createEnvCommand)
+        executeShellCommand(activateEnvCommand)
+        executeShellCommand(snykTestCommand)
+    }
+
+    static void executeShellCommand(String command) {
         def output = command.execute().text
-        println "Shell Output: $output"
+        println "Shell Command: $command\nOutput: $output"
     }
 }
