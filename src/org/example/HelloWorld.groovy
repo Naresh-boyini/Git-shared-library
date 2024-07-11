74f1fbce7651df5097e0a6233c5520655105173d
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-12 02:55:09 +0530

    Update HelloWorld.groovy

--- src/org/example/HelloWorld.groovy
+++ src/org/example/HelloWorld.groovy
@@ -3,23 +3,29 @@ package org.example
 
 class HelloWorld {
     static void sayHello() {
-        // Command to create a Python virtual environment
-        def createEnvCommand = "python3 -m venv myenv"
-        
-        // Command to activate the virtual environment
-        def activateEnvCommand = ". myenv/bin/activate"
-        
-        // Command to run Snyk security test
-        def snykTestCommand = "snyk test --file=poetry.lock"
-        
-        // Execute commands sequentially
-        executeShellCommand(createEnvCommand)
-        executeShellCommand(activateEnvCommand)
-        executeShellCommand(snykTestCommand)
+        try {
+            // Command to create a Python virtual environment
+            def createEnvCommand = "python3 -m venv myenv"
+
+            // Command to activate the virtual environment
+            def activateEnvCommand = ". myenv/bin/activate"
+
+            // Execute commands sequentially
+            executeShellCommand(createEnvCommand)
+            executeShellCommand(activateEnvCommand)
+        } catch (Exception e) {
+            println "Error executing command: ${e.message}"
+            e.printStackTrace()
+        }
     }
 
     static void executeShellCommand(String command) {
-        def output = command.execute().text
-        println "Shell Command: $command\nOutput: $output"
+        try {
+            def output = command.execute().text.trim()
+            println "Shell Command: $command\nOutput: $output"
+        } catch (Exception e) {
+            println "Error executing command: ${e.message}"
+            e.printStackTrace()
+        }
     }
 }
