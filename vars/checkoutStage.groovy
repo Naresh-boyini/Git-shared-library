6d16c4a46c9999aab4d2c88ee1b8a0b1858bf2f1
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-13 18:17:37 +0530

    Update checkoutStage.groovy

--- vars/checkoutStage.groovy
+++ vars/checkoutStage.groovy
@@ -1,3 +1,10 @@
-def call() {
-    org.example.Checkout.execute(this)
+def call(Map pipelineParams) {
+    stage('Checkout') {
+        org.example.Checkout.execute(this)
+    }
+
+    stage('Build') {
+        def projectDirectory = pipelineParams.projectDirectory ?: error('Missing projectDirectory parameter')
+        org.example.Build.execute(this, projectDirectory)
+    }
 }
