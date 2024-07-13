44028b8b2fa1c0402e70d6c9c5fca68805813ade
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-13 19:27:28 +0530

    Update Checkout.groovy

--- src/org/example/Checkout.groovy
+++ src/org/example/Checkout.groovy
@@ -1,11 +1,13 @@
+// src/org/example/Checkout.groovy
+
 package org.example
 
 class Checkout {
-    static void execute(script) {
+    static void execute(script, String gitUrl, String gitBranch) {
         script.checkout([
             $class: 'GitSCM',
-            branches: [[name: script.params.GIT_BRANCH]],
-            userRemoteConfigs: [[url: script.params.GIT_URL]]
+            branches: [[name: gitBranch]],
+            userRemoteConfigs: [[url: gitUrl]]
         ])
     }
 }
