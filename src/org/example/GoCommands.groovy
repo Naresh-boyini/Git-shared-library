bb7f8bf2ec8be31f7b411f53c8fb0d86b1b72015
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-14 01:15:06 +0530

    Update GoCommands.groovy

--- src/org/example/GoCommands.groovy
+++ src/org/example/GoCommands.groovy
@@ -6,8 +6,8 @@ class GoCommands {
        // def goTool = tool name: 'Go', type: 'hudson.plugins.go.GoInstallation'
 
         // Use Go tool
-        script.withEnv(["PATH+GO=${goTool}/bin"]) {
-            script.sh '''
+      //  script.withEnv(["PATH+GO=${goTool}/bin"]) {
+          sh '''
                 go mod tidy
                 go mod download
                 go build -o employee-api .
