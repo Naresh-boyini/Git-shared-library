6cd7ffb47a214f8fcfb6f2b98e12444edc88d4cf
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-14 01:14:10 +0530

    Update GoCommands.groovy

--- src/org/example/GoCommands.groovy
+++ src/org/example/GoCommands.groovy
@@ -3,7 +3,7 @@ package org.example
 class GoCommands {
     static void execute(script) {
         // Define Go tool
-        def goTool = tool name: 'Go', type: 'hudson.plugins.go.GoInstallation'
+       // def goTool = tool name: 'Go', type: 'hudson.plugins.go.GoInstallation'
 
         // Use Go tool
         script.withEnv(["PATH+GO=${goTool}/bin"]) {
