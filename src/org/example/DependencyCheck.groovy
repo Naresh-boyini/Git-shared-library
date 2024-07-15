45185583c71f50e3c5a10304b5499f597b1c8fa9
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-16 00:51:08 +0530

    Update DependencyCheck.groovy

--- src/org/example/DependencyCheck.groovy
+++ src/org/example/DependencyCheck.groovy
@@ -2,9 +2,7 @@ package org.example
 
 class DependencyCheck {
     static void execute(script) {
-        def dependencyCheckHome = tool name: 'Dependency-Check', type: 'org.jenkinsci.plugins.DependencyCheck.DependencyCheckToolInstaller'
-        
-        // Run Dependency-Check using the defined tool installation
+                // Run Dependency-Check using the defined tool installation
         script.withEnv(['DEPENDENCY_CHECK_HOME': dependencyCheckHome]) {
             script.sh '''
                 ${DEPENDENCY_CHECK_HOME}/bin/dependency-check.sh --scan . --format ALL
