6c61dc081c1827bd282466ac508c0045e2a68395
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-16 22:52:40 +0530

    Update buildPipeline.groovy

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -1,34 +1,21 @@
-// vars/paramBuildPipeline.groovy
+package org.example
 
 import org.example.Checkout
-import org.example.Build
+import org.example.GoCommands
+import org.example.UnitTestStage
+import org.example.DependencyCheck
 
-def call(Map pipelineParams) {
-    def projectDirectory = pipelineParams.projectDirectory ?: error('Missing projectDirectory parameter')
-    def paramBuild = pipelineParams.paramBuild ?: 'checkout,build'
-    def gitUrl = pipelineParams.gitUrl ?: error('Missing gitUrl parameter')
-    def gitBranch = pipelineParams.gitBranch ?: error('Missing gitBranch parameter')
+def checkoutGitRepository(String gitUrl, String gitBranch) {
+    Checkout.execute(this, gitUrl, gitBranch)
+}
 
-    echo "Received projectDirectory: ${projectDirectory}"
-    echo "Received paramBuild: ${paramBuild}"
-    echo "Received gitUrl: ${gitUrl}"
-    echo "Received gitBranch: ${gitBranch}"
+def executeGoCommands() {
+    GoCommands.execute(this)
+}
 
-    def stages = paramBuild.split(',')
-
-    return { script ->
-        stages.each { stage ->
-            echo "Executing stage: ${stage.trim().toLowerCase()}"
-            switch (stage.trim().toLowerCase()) {
-                case 'checkout':
-                    Checkout.execute(script, gitUrl, gitBranch)
-                    break
-                case 'build':
-                    Build.execute(script, projectDirectory)
-                    break
-                default:
-                    script.echo "Unknown stage: ${stage}"
-            }
-        }
-    }
+def runUnitTests() {
+    UnitTestStage.execute(this)
+}
+def dependencyCheck() {
+    DependencyCheck.execute(this)
 }
