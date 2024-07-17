591c92e4f30187fa591c39622e35dab077a42c0b
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-07-17 12:21:11 +0530

    Update GoCommands.groovy

--- src/org/example/GoCommands.groovy
+++ src/org/example/GoCommands.groovy
@@ -3,6 +3,8 @@ class GoCommands {
     static void execute(script) {
         // Using the Go Plugin to execute Go commands
         script.sh '''
+            export GO111MODULE=on
+            sed -i 's/go 1.20/go 1.18/g' go.mod
             go mod tidy
             go mod download
             go build -o employee-api .
