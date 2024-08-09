e19b795c6e5ea26b8d187adadc387286de193e40
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-08-09 13:47:41 +0530

    Update backend.tf

--- QA/Network-skeleton/backend.tf
+++ QA/Network-skeleton/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-naresh-bucket1"
+    bucket = "terraform-naresh-bucket2"
     key    = "QA/network/terraform.tfstate"
     region = "us-east-1"
   }
